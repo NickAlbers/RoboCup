5559510027143c7a6a7a5b1c648d726084a2c960
Nick Albers <nickalbers94@gmail.com>
2015-07-24 13:24:24 +1200

    XboxController6
    Working Diff, Track Speed, and Xbox controller

--- RobocupMain/RobocupMain.ino
+++ RobocupMain/RobocupMain.ino
@@ -60,17 +60,25 @@ void loop()
         
         //Read joystick values
         int trackSpeed = Xbox.getAnalogHat(LeftHatY, 0);
-        
+        int trackDiff = Xbox.getAnalogHat(RightHatX, 0);
         //Map to motor ranges
        trackSpeed  = map(trackSpeed, -32767, 32767, 135, 45); //Flip these if the chasis is flipped
+       trackDiff = map(trackDiff, -32767, 32767, 45, -45);
        
        //Deadband for Trackspeeds between 85 and 95
        if (trackSpeed < 100 && trackSpeed > 80) {trackSpeed = 90;}
+
+       //Having one plus and one minus should facilitate movement in opposite
+       //directions. Right on the RightHatX will increase left track speed and
+       //slow right track, vice versa for the other direction
+       int trackLeft = trackSpeed + trackDiff; 
+       int trackRight = trackSpeed - trackDiff;
        
        //Write output values to the motor
-       leftServo.write(trackSpeed);
-       rightServo.write(trackSpeed);
-       Serial.println(trackSpeed);
+       leftServo.write(trackLeft);
+       rightServo.write(trackRight);
+       Serial.println(trackLeft);
+       Serial.println(trackRight);
       }
 
       if (Xbox.getButtonClick(UP, 0)) {
