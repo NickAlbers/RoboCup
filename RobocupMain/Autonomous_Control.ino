005edc881adc1c1a8044cd703f3b7a2ff347fd23
Nick Albers <nickalbers94@gmail.com>
2015-07-27 13:27:59 +1200

    Disabled Autonomous
    Commented out random drive case statements

--- RobocupMain/Autonomous_Control.ino
+++ RobocupMain/Autonomous_Control.ino
@@ -5,32 +5,32 @@ void autonomousControl() {
   static int autoState = 0;
   static long nextRun = 0; 
   Serial.println("Hello World");
-  
-  //Can I run yet?
-  if (millis() < nextRun)
-    return;
-    
-  switch (autoState) {
-    case 0:
-      leftServo.write(135);
-      rightServo.write(135);
-      break;
-    case 1:
-      leftServo.write(90);
-      rightServo.write(90);
-      break;
-    case 2:
-      leftServo.write(45);
-      rightServo.write(45);
-      break;
-    case 3:
-      leftServo.write(90);
-      rightServo.write(90);
-      break;
-  }
-  
-  autoState = (autoState + 1) % 4;
-  nextRun = millis() + 1500;
+//  
+//  //Can I run yet?
+//  if (millis() < nextRun)
+//    return;
+//    
+//  switch (autoState) {
+//    case 0:
+//      leftServo.write(135);
+//      rightServo.write(135);
+//      break;
+//    case 1:
+//      leftServo.write(90);
+//      rightServo.write(90);
+//      break;
+//    case 2:
+//      leftServo.write(45);
+//      rightServo.write(45);
+//      break;
+//    case 3:
+//      leftServo.write(90);
+//      rightServo.write(90);
+//      break;
+//  }
+//  
+//  autoState = (autoState + 1) % 4;
+//  nextRun = millis() + random(500, 4000);
 }
 
 
